549a33fb6cdac0d993f886db006d4253939448c6
Tran Duc Thanh <thanhtranintel@yahoo.com>
2013-06-08 11:39:14 +0700

    StringCalculator 7_6

---
+++ StringCalculator 7_6/src/main/java/StringCalculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package StringCalculator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thanhtd
+ * Date: 08/06/2013
+ * Time: 11:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringCalculator {
+}
