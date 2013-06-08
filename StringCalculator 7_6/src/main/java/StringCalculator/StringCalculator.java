d4b24192d4f31ec7ab2f090339c62204567fd6e5
Tran Duc Thanh <thanhtranintel@yahoo.com>
2013-06-08 11:47:54 +0700

    returnZero Green

--- StringCalculator 7_6/src/main/java/StringCalculator/StringCalculator.java
+++ StringCalculator 7_6/src/main/java/StringCalculator/StringCalculator.java
@@ -9,4 +9,10 @@ package StringCalculator;
  */
 public class StringCalculator {
 
+    public static int add(String str) {
+        if(str.isEmpty()){
+            return 0;
+        }
+        return 0;
+    }
 }
