d2c008dd36417450f7dc0a33ba5a307a13908bf2
Tran Duc Thanh <thanhtranintel@yahoo.com>
2013-06-13 11:04:52 +0700

    BankAccount

---
+++ BankAccount/src/main/java/BankAccountDAO.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: thanhtd
+ * Date: 12/06/2013
+ * Time: 15:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class BankAccountDAO {
+    public BankAccountDTO save(BankAccountDTO account){
+        return null;
+    }
+    public BankAccountDTO findByAccountNumber(String strAccountNumber){
+        return null;
+    }
+    public BankAccountDTO save(BankAccountDTO capture, Long capture1) {
+        return null;
+    }
+}
