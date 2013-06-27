1c4e2569188a45d185f7111ab19c379c946ea9f0
Tran Duc Thanh <thanhtranintel@yahoo.com>
2013-06-27 07:49:34 +0700

    Red test one

---
+++ BankAccount_26_6/src/test/java/TestBankAccount.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thanhtd
+ * Date: 27/06/2013
+ * Time: 07:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBankAccount {
+    BankAccountDao mockAccountDao = mock(BankAccountDao.class);
+    Calendar mockCalendar = mock(Calendar.class);
+    private String accountNumber = "1234567890";
+
+    @Before
+    public void setUp(){
+        reset(mockAccountDao);
+        BankAccount.setBankAccountDao(mockAccountDao);
+    }
+
+    @Test
+    public void testCreateNewAccountHasBalanceZero(){
+        ArgumentCaptor<BankAccountDTO> argumentCaptor = ArgumentCaptor.forClass(BankAccountDTO.class);
+        BankAccountDTO.setCalendar(mockCalendar);
+        when(mockCalendar.getTimeInMillis()).thenReturn(1000L);
+
+        BankAccountDTO newAcc = BankAccount.openAccount(accountNumber);
+        verify(mockAccountDao).save(argumentCaptor.capture());
+
+        assertEquals(accountNumber, argumentCaptor.getValue().getAccountNumber());
+        assertEquals(argumentCaptor.getValue().getBalance(),0,0.01);
+        assertEquals(argumentCaptor.getValue().getOpenTimeStamp(),1000L);
+    }
+}
